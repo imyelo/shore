d1212b2ab8f7b55d85af1cdad549d4d75e5e9a26
yelo <zhihuzeye@gmail.com>
2015-05-26 17:40:17 +0800

    add limit to all api

--- app/server/route/api.js
+++ app/server/route/api.js
@@ -1,13 +1,13 @@
 var api = require('../ctrl/api');
 var filter = require('../ctrl/filter');
-var auth = require('koa-basic-auth');
 var config = require('config');
+var auth = require('koa-basic-auth')({ name: config.self.AUTH.NAME, 'pass': config.self.AUTH.PASS});
 
 module.exports = function (app) {
-  app.get('/api/yacht', filter.ip, filter.auth, auth({ name: config.self.AUTH.NAME, 'pass': config.self.AUTH.PASS}), api.list);
-  app.get('/api/yacht/:id', api.detail);
-  app.post('/api/yacht', api.create);
-  app.post('/api/yacht/:id', api.update);
-  app.delete('/api/yacht/:id', api.remove);
-  app.delete('/api/yacht', api.destroy);
+  app.get('/api/yacht', filter.ip, filter.auth, auth, api.list);
+  app.get('/api/yacht/:id', filter.ip, filter.auth, auth, api.detail);
+  app.post('/api/yacht', filter.ip, filter.auth, auth, api.create);
+  app.post('/api/yacht/:id', filter.ip, filter.auth, auth, api.update);
+  app.delete('/api/yacht/:id', filter.ip, filter.auth, auth, api.remove);
+  app.delete('/api/yacht', filter.ip, filter.auth, auth, api.destroy);
 };
