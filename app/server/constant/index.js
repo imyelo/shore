8f737e8e4853eb94c0d410d51f1ff35096afb1b6
yelo <zhihuzeye@gmail.com>
2015-05-26 17:37:29 +0800

    update limit

--- app/server/constant/index.js
+++ app/server/constant/index.js
@@ -1,6 +1,5 @@
 module.exports = {
   AUTH_CACHE_KEY_PREFIX: 'shore_auth_',
-  // AUTH_CACHE_EXPIRE: 60 * 60
-  AUTH_MAX_FAIL_TIMES: 5,
-  AUTH_CACHE_EXPIRE: 10
+  AUTH_MAX_FAIL_TIMES: 10,
+  AUTH_CACHE_EXPIRE: 24 * 60 * 60
 };
